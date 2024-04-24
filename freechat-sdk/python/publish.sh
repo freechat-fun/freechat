b8441d2c195b31fa96fd8cf7e3e6f2211e1eaa60
freechat <dev.freechat.fun@gmail.com>
2024-04-24 19:10:40 +0800

    Fix An Issue in ./gen-sdk.sh

--- freechat-sdk/python/publish.sh
+++ freechat-sdk/python/publish.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
-npm install
-npm run build
-npm publish
+set -eux
+
+ver=$(sed -n 's/__version__ = "\([a-zA-Z0-9.-]\{1,\}\)"/\1/p' freechat-sdk/__init__.py | head -1)
+
+python3 setup.py clean
+python3 setup.py bdist_wheel
+python3 setup.py sdist
+
+twine check dist/freechat_sdk-${ver}-py3-none-any.whl
+twine upload dist/freechat_sdk-${ver}-py3-none-any.whl --verbose
