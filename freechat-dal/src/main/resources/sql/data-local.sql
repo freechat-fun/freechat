39020d5364156d08b9a7a86b0c843f5edb7faf15
freechat <dev.freechat.fun@gmail.com>
2024-11-13 10:36:29 +0800

    refactor: replace 'value' with 'values' in sql for standard sql syntax

--- freechat-dal/src/main/resources/sql/data-local.sql
+++ freechat-dal/src/main/resources/sql/data-local.sql
@@ -1,5 +1,5 @@
 use `freechat`;
 
-INSERT INTO `user` (`gmt_create`, `gmt_modified`, `user_id`, `username`, `password`, `nickname`) VALUE ('2024-05-01 19:11:32', '2024-05-01 19:11:32', '1', 'admin', 'J+qPgqqyiwRezG/XLN0LSQ==', 'Administrator');
-INSERT INTO `authority` (`gmt_create`, `gmt_modified`, `user_id`, `scope`)  VALUE ('2024-05-01 19:11:32', '2024-05-01 19:11:32', '1', 'ROLE_ADMIN');
-INSERT INTO `api_token` (`gmt_create`, `gmt_modified`, `user_id`, `token`, `policy`, `issued_at`) VALUE ('2024-05-01 19:11:32', '2024-05-01 19:11:32', '1', 'fc-cac2e3c7f6ca4f5b9c5d7e0da074f4f7', '*', '2024-05-01 19:11:32');
+INSERT INTO `user` (`gmt_create`, `gmt_modified`, `user_id`, `username`, `password`, `nickname`) VALUES ('2024-05-01 19:11:32', '2024-05-01 19:11:32', '1', 'admin', 'J+qPgqqyiwRezG/XLN0LSQ==', 'Administrator');
+INSERT INTO `authority` (`gmt_create`, `gmt_modified`, `user_id`, `scope`)  VALUES ('2024-05-01 19:11:32', '2024-05-01 19:11:32', '1', 'ROLE_ADMIN');
+INSERT INTO `api_token` (`gmt_create`, `gmt_modified`, `user_id`, `token`, `policy`, `issued_at`) VALUES ('2024-05-01 19:11:32', '2024-05-01 19:11:32', '1', 'fc-cac2e3c7f6ca4f5b9c5d7e0da074f4f7', '*', '2024-05-01 19:11:32');
