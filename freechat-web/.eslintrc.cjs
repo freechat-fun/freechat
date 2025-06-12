13b3927498e1e950bcb63a55d0da79458f275cd0
freechat <dev.freechat.fun@gmail.com>
2025-06-12 18:24:41 +0800

    refactor: abandon eslint-config-react-app

--- freechat-web/.eslintrc.cjs
+++ freechat-web/.eslintrc.cjs
@@ -4,13 +4,18 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [
+    'react-refresh',
+  ],
   rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
