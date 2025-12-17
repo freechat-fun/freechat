5f8f81fc8da92821865af5e0f9015e4e66c608bf
freechat <dev.freechat.fun@gmail.com>
2025-12-17 12:19:23 +0800

    fix: specify the async request timeout in Spring MVC and Tomcat

--- freechat-start/src/main/java/fun/freechat/config/TomcatConfig.java
+++ freechat-start/src/main/java/fun/freechat/config/TomcatConfig.java
@@ -1,17 +1,22 @@
 package fun.freechat.config;
 
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.tomcat.servlet.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @SuppressWarnings("unused")
 public class TomcatConfig {
+    @Value("${spring.mvc.async.requestTimeout}")
+    private long asyncRequestTimeout;
 
     @Bean
     public TomcatServletWebServerFactory tomcatServletWebServerFactory() {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.addConnectorCustomizers(connector -> connector.setAsyncTimeout(300_000L)); // Set async timeout to 5 minutes
+        factory.addConnectorCustomizers(
+                connector -> connector.setAsyncTimeout(asyncRequestTimeout)
+        );
         return factory;
     }
 }
